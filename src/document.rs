4715249cbff97e7a414c4eba8ec26e91abc271e4
Esraa Jbara <jbesraa@gmail.com>
2020-10-17 14:58:25 +0300

    insert text functionality

--- src/document.rs
+++ src/document.rs
@@ -1,34 +1,45 @@
+use crate::Position;
 use crate::Row;
 use std::fs;
 
 #[derive(Default)]
 pub struct Document {
-	rows: Vec<Row>,
-	pub file_name: Option<String>,
+    rows: Vec<Row>,
+    pub file_name: Option<String>,
 }
 
 impl Document {
-	pub fn open(filename: &str) -> Result<Self, std::io::Error> {
-		let contents = fs::read_to_string(filename)?;
-		let mut rows = Vec::new();
-		for value in contents.lines() {
-			rows.push(Row::from(value));
-		}
-		Ok(Self {
-			rows,
-			file_name: Some(filename.to_string()),
-		})
-	}
+    pub fn open(filename: &str) -> Result<Self, std::io::Error> {
+        let contents = fs::read_to_string(filename)?;
+        let mut rows = Vec::new();
+        for value in contents.lines() {
+            rows.push(Row::from(value));
+        }
+        Ok(Self {
+            rows,
+            file_name: Some(filename.to_string()),
+        })
+    }
 
-	pub fn is_empty(&self) -> bool {
-		self.rows.is_empty()
-	}
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
 
-	pub fn row(&self, index: usize) -> Option<&Row> {
-		self.rows.get(index)
-	}
+    pub fn row(&self, index: usize) -> Option<&Row> {
+        self.rows.get(index)
+    }
 
-	pub fn len(&self) -> usize {
-		self.rows.len()
-	}
+    pub fn len(&self) -> usize {
+        self.rows.len()
+    }
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y == self.len() {
+            let mut row = Row::default();
+            row.insert(0, c);
+            self.rows.push(row);
+        } else if at.y < self.len() {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.insert(at.x, c)
+        }
+    }
 }
